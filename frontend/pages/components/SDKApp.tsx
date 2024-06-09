b506d30f15bb3c0d1637754367f608aee467d3fa
kkbvac <f.morisan@gmail.com>
2024-06-09 12:23:01 -0300

    fix: make baseUri depend on deployment location

--- frontend/pages/components/SDKApp.tsx
+++ frontend/pages/components/SDKApp.tsx
@@ -8,16 +8,16 @@ export default function SDKApp(props: any) {
     const account = useAccount()
     const transactor = useSendTransaction()
 
-    const [baseUri, setBaseUri] = useState<string>('http://localhost:8000/main')
+    const [baseUri, setBaseUri] = useState<string>("")
     const [position, setPosition] = useState<number>(0)
     const [price, setPrice] = useState<number>(0)
 
     useEffect(() => {
-        setBaseUri(`http://localhost:8000/${account.chainId === 1? 'main': 'opt'}`)
+        setBaseUri(`${window.location.protocol}//${window.location.host}/${account.chainId === 1? 'main': 'opt'}`)
     }, [account.chainId])
 
     useEffect(() => {
-        if (!account.address) return
+        if (!account.address || !baseUri) return
         fetch(`${baseUri}/position`, {
             method: "POST",
             body: JSON.stringify({address: account.address})
@@ -38,8 +38,10 @@ export default function SDKApp(props: any) {
             <p>price {price}</p>
             <p>position {position}</p>
             <span>{props.address}</span>
-            <SDKDeposit baseUri={baseUri} />
-            <SDKWithdraw baseUri={baseUri} />
+            {baseUri && <>
+                <SDKDeposit baseUri={baseUri} />
+                <SDKWithdraw baseUri={baseUri} />
+            </>}
         </div>
     )
 }
